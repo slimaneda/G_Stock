5213e02d88b61517f40507203f27875cf78a7d48
slimane dany <slimanedany@gmail.com>
2023-09-15 21:57:04 +0100

    dotos

--- forms/Depts.vb
+++ forms/Depts.vb
@@ -1,8 +1,5 @@
 ﻿Public Class Depts
     Dim _Class_depts As New Class_depts
-    Private Sub Depts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
         _Class_depts.inserdata("search_code_fact", Val(TextBox3.Text), DGV1)
